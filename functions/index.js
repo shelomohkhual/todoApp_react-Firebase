4f4759355abd29f9a19edd47717962f15bd47423
shelomoh <shelomoh.design@gmail.com>
2020-05-11 01:44:14 +0800

    re-setup with firebase

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,8 @@
-const functions = require('firebase-functions');
-const app = require('express')();
-const auth = require('./util/auth');
- 
-const {
-    getAllTodos,
-    getOneTodo,
-    postOneTodo,
-    deleteTodo,
-    editTodo
-} = require('./APIs/todos')
+const functions = require("firebase-functions");
 
-const { 
-    loginUser,
-    signUpUser,
-    uploadProfilePhoto,
-    getUserDetail,
-    updateUserDetails
-} = require('./APIs/users')
-
-// Todos
-app.get('/todos', auth, getAllTodos);
-app.get('/todo/:todoId', auth, getOneTodo);
-app.post('/todo',auth, postOneTodo);
-app.delete('/todo/:todoId',auth, deleteTodo);
-app.put('/todo/:todoId',auth, editTodo);
-
-// Users
-app.post('/login', loginUser);
-app.post('/signup', signUpUser);
-app.post('/user/image', auth ,uploadProfilePhoto);
-app.post('/user', auth ,updateUserDetails);
-app.get('/user', auth, getUserDetail);
-
-exports.api = functions.https.onRequest(app);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});

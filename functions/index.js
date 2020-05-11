88cc196299f9d908147510f5ab45ad85dc383297
shelomoh <shelomoh.design@gmail.com>
2020-05-11 16:47:41 +0800

    add & init "express"

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,7 @@
 const functions = require("firebase-functions");
+const app = require("express")();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+const { getAllTodos } = require("./APIs/todos");
+
+app.get("/todos", getAllTodos);
+exports.api = functions.https.onRequest(app);

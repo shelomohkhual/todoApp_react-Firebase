57179c21a42e687567feb5be2c67595b9255a04d
shelomoh <shelomoh.design@gmail.com>
2020-05-30 19:15:43 +0800

    implement frontend section

---
+++ view/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import login from "./pages/login";
+import signup from "./pages/signup";
+import home from "./pages/home";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
+          <Route exact path="/" component={home} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

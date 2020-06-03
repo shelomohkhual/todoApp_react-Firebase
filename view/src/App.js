413e5290e563e0bc075960059f2b0c61edb9628c
shelomoh <shelomoh.design@gmail.com>
2020-06-03 22:09:10 +0800

    add theme to the app

--- view/src/App.js
+++ view/src/App.js
@@ -4,17 +4,33 @@ import login from "./pages/login";
 import signup from "./pages/signup";
 import home from "./pages/home";
 
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#FF5722",
+      dark: "#d50000",
+      contrastText: "#fff",
+    },
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/login" component={login} />
-          <Route exact path="/signup" component={signup} />
-          <Route exact path="/" component={home} />
-        </Switch>
-      </div>
-    </Router>
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+            <Route exact path="/" component={home} />
+          </Switch>
+        </div>
+      </Router>
+    </MuiThemeProvider>
   );
 }
 

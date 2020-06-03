6cd4bd6999f705464cf71b871df195a778ca5f57
shelomoh <shelomoh.design@gmail.com>
2020-06-04 05:34:45 +0800

    mergin be and fe directory

---
+++ build/precache-manifest.9000cbde2d68788e5ed3a7bf13a95b34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447bd07b3b5d5dde9d1f1db15bf2a0d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ed68fd53cd5bba442a3",
+    "url": "/static/js/2.a8e1b6b6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a8e1b6b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539ace3c3123c7503d22",
+    "url": "/static/js/main.b1dd76f2.chunk.js"
+  },
+  {
+    "revision": "cc57d6e3b4fc5a895001",
+    "url": "/static/js/runtime-main.7081cf4f.js"
+  }
+]);
